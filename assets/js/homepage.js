78cecb8923d48bee67d4abcacf7ee8361f84ad4c
Sandra Sanders <sandy-sanders@msn.com>
2022-04-10 09:18:56 -0700

    6.2.5

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,33 @@
-var getUseRepos = function() {
-    fetch('https://api.github.com/users/octocat/repos');
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    //get value from input element
+    var username = nameInputEl.value.trim();
+
+    if(username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    
+  };
+
+var getUserRepos = function(user) {
+   // format the github api url
+   var apiUrl = "https://api.github.com/users/"  + user + "/repos";
+
+   //make a request to the url
+   fetch(apiUrl).then(function(response) {
+       response.json().then(function(data) {
+           console.log(data);
+       });
+   });
+    
 };
 
 
-getUseRepos();
+
+userFormEl.addEventListener("submit", formSubmitHandler);
