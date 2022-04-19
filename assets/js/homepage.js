1649eb960535eb46a24f98539054a6b0caf4642e
Sandra Sanders <sandy-sanders@msn.com>
2022-04-18 21:06:43 -0700

    show repos and issues on page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,7 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
-var repoContainerEl = document.querySelector("repos-container");
-var reposSearchTerm = document.querySelector("repo-search-term");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -32,7 +32,38 @@ var getUserRepos = function(user) {
 
 var displayRepos = function(repos, searchTerm) {
     console.log(repos);
-    console.log(searchTerm);
+    console.log(searchTerm); 
+    // clear old content
+repoContainerEl.textContent = "";
+repoSearchTerm.textContent = searchTerm;
+// loop over repos
+for (var i = 0; i < repos.length; i++) {
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+    var repoEl = document.createElement("div");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+
+    repoEl.appendChild(titleEl);
+
+    //creat a status element
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+
+    if (repos[i].open_issues_count > 0) {
+        statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + "issue(s)";
+    } else {
+        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+    repoEl.appendChild(statusEl);
+
+    repoContainerEl.appendChild(repoEl);
+
+}
+
+
 };
 
 
